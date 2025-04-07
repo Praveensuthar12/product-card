a69753ffdc3559eb377f2d71314f18692642f9ba
PraveenSuthar26 <155362413+PraveenSuthar26@users.noreply.github.com>
2025-04-07 14:30:25 +0530

    Products-card

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Product from './components/Product';
+
+function App() {
+
+  return (
+    <>
+      <h1>Product</h1>
+      <Product />
+    </>
+  );
+}
+
+export default App
